842f3ff626ecc2eaceeda7c2165f1aaf10992afc
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-03-30 16:18:21 +0200

    added delete button

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,16 +2,17 @@
 const createAccountButton = document.getElementById("create-account-button") as HTMLButtonElement | null;
 const submitButton = document.getElementById("submit-button") as HTMLButtonElement | null;
 const usernameInput = document.getElementById("username") as HTMLInputElement | null;
-const PasswordInput = document.getElementById("password") as HTMLInputElement | null;
 const statusInput = document.getElementById("status") as HTMLInputElement | null;
 const imageUrlInput = document.getElementById("image-url") as HTMLInputElement | null;
-const userNameInput = document.getElementById("user-name") as HTMLInputElement | null;
 const passwordInput = document.getElementById("password") as HTMLInputElement | null;
 const confirmPasswordInput = document.getElementById("confirm-password") as HTMLInputElement | null;
+const form = document.getElementById('form') as HTMLFormElement | null;
+const errorMessage = document.createElement("p");
+errorMessage.id = "error-message";
 
 
 interface UserInfo {
-    userName: string;
+    userName: any;
     password: string;
     status: string;
     imageurl: string;
@@ -27,8 +28,8 @@ const baseUrl = "https://social-media-68d76-default-rtdb.europe-west1.firebaseda
 
 async function getUsers(): Promise<UserInfo[]> {
     try {
-        console.log("Fetching users...");
-        const response = await fetch(`${baseUrl}.json`);
+        console.log("Fetching users");
+        const response = await fetch(`${baseUrl}users.json`); // Change this line
 
         if (!response.ok) {
             throw new Error(`Error: ${response.status} ${response.statusText}`);
@@ -43,13 +44,15 @@ async function getUsers(): Promise<UserInfo[]> {
 
         console.log(users);
         const usersArray: UserInfo[] = Object.values(users);
-        const newOrOldUsers: UserInfo[] = usersArray.filter((user) => user.newUser ?? true);
-        return newOrOldUsers;
+        console.log(usersArray);
+        return usersArray;
+
     } catch (err) {
         console.log(err);
-        throw new Error("Failed to fetch users.");
+        throw new Error("Failed to fetch users");
     }
 }
+
 
 //uppdate the users array
 async function saveUser(user: UserInfo): Promise<void> {
@@ -82,65 +85,115 @@ async function saveUser(user: UserInfo): Promise<void> {
 
 // Event listener for the "create account" button
 if (createAccountButton && usernameInput && passwordInput && statusInput && imageUrlInput) {
-  createAccountButton.addEventListener("click", async () => {
-    const userName = usernameInput.value;
-    const isAvailable = await isUsernameAvailable(userName);
-
-    if (!isAvailable) {
-      const errorMessage = document.createElement("p");
-      errorMessage.textContent = "Username is already taken. Please choose another one.";
-      errorMessage.style.color = "red";
-      createAccountButton.insertAdjacentElement("afterend", errorMessage);
-      return;
-    }
-
-    const userInfo: UserInfo = {
-      userName: userName,
-      password: passwordInput.value,
-      status: statusInput.value,
-      imageurl: imageUrlInput.value,
-      newUser: true,
-    };
-
-    await saveUser(userInfo);
-  });
-} else {
-  console.error("One or more DOM elements not found.");
-}
-
-async function isUsernameAvailable(username: string): Promise<boolean> {
-  const users = await getUsers();
-  return !users.some((user) => user.userName === username);
-}
-
-
-// Event listener for the "Login" button
-
-if (submitButton && usernameInput) {
-    submitButton.addEventListener("click", async (event: MouseEvent) => {
-        event.preventDefault();
-
-        if (!passwordInput) {
-            console.error("Wrong password");
+    createAccountButton.addEventListener("click", async () => {
+        errorMessage.textContent = " ";
+        const userName = usernameInput.value;
+        const isAvailable = await isUsernameAvailable(userName);
+        if (!isAvailable) {
+            errorMessage.textContent = "Username is already taken. Please choose another one.";
+            errorMessage.style.color = "red";
+            createAccountButton.insertAdjacentElement("afterend", errorMessage);
             return;
         }
 
+        const userInfo: UserInfo = {
+            userName: userName,
+            password: passwordInput.value,
+            status: statusInput.value,
+            imageurl: imageUrlInput.value,
+            newUser: true,
+        };
+
+        await saveUser(userInfo);
+    });
+} else {
+    console.error("One or more DOM elements not found.");
+}
+
+//control already existing users
+async function isUsernameAvailable(username: string): Promise<boolean> {
+    const users = await getUsers();
+    //checks if there's any user in the array with the same username using the some() method.
+    return !users.some((user) => user.userName === username);
+}
+
+
+
+/// Event listener for the "submit" button
+if (submitButton && usernameInput && passwordInput) {
+    submitButton.addEventListener("click", async (event: MouseEvent) => {
+        event.preventDefault();
+        // Remove error message if it exists
+        errorMessage.textContent = " ";
         const password = passwordInput.value;
         const users = await getUsers();
         const user = users.find((u) => u.userName === usernameInput.value);
+        errorMessage.style.color = " ";
+        //Log in div for user page
+        // form!.style.display = "none";
+        // const logInpage = document.createElement('div');
+        // document.body.appendChild(logInpage);
+        // logInpage.innerHTML = `<h1>Welcome!</h1>`;
 
         if (!user) {
             const errorMessage = document.createElement("p");
             errorMessage.textContent = "No account found for this user. Please create an account first.";
             errorMessage.style.color = "red";
-            submitButton.insertAdjacentElement("afterend", errorMessage);
+            form?.appendChild(errorMessage);
+            return;
+        }
+
+        // Add password check
+        else if (user.password !== password) {
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = "Incorrect password. Please try again.";
+            errorMessage.style.color = "red";
+            form?.appendChild(errorMessage);
             return;
         }
 
         user.newUser = false;
         await saveUser(user);
-        window.location.href = "successLogin.html"; // replace "new-page.html" with the name of the page you want to redirect to
+
     });
 } else {
     console.error("One or more DOM elements not found.");
 }
+
+// //Event listener for delete button
+const deleteButton = document.getElementById('delete-account-button');
+deleteButton?.addEventListener("click", async (event) => {
+    event?.preventDefault();
+    if (usernameInput) {
+        await deleteUser(usernameInput.value);
+    } else {
+        console.error("Username input element not found.");
+    }
+});
+
+
+
+async function deleteUser(username: string): Promise<void> {
+    console.log("Deleting user");
+    const url = `${baseUrl}users/${username}.json`;
+    const init = {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    };
+
+    try {
+        const response = await fetch(url, init);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        console.log("User deleted successfully");
+    } catch (err) {
+        console.log(err);
+        throw new Error("Failed to delete user.");
+    }
+}
+
+
